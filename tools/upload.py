0b10d3dc6cef0fae638d1581f3986d3e73cda8d9
Chen2x <chenchen199727@gmail.com>
2017-06-01 15:25:25 -0400

    testing without metadata

--- tools/upload.py
+++ tools/upload.py
@@ -6,7 +6,7 @@ import subprocess
 from utils import get_task_dict, save_output_json
 
 #task_dict = get_task_dict(sys.argv[1])
-task_dict = get_task_dict("""{"input": {"project_code": "23423","collab_file_id": "2341","file_name": "data/bc1b1071490d454e5db5133f1e679d9c.CPCG_0047_Ly_R_PE_294_WG_111216_h801_0065_AD0813ACXX_6_NoIndex_R1.fastq.gz","file_md5sum": "sdfs","object_id": "fbd35588-5bf8-560c-873a-0410f49e5748"}}""")
+task_dict = get_task_dict("""{"input": {"project_code": "23423","collab_file_id": "2341","file_name": "data/test","file_md5sum": "sdfs","object_id": "fbd35588-5bf8-560c-873a-0410f49e5748"}}""")
 cwd = os.getcwd()
 
 """
@@ -32,12 +32,12 @@ project_code = task_dict.get('input').get('project_code')
 task_start = int(time.time())
 
 try:
-    print subprocess.check_output(['icgc-storage-client','upload','--file', file_name, '--object-id', 'fbd35588-5bf8-560c-873a-0410f49e5748' ])
+    print subprocess.check_output(['icgc-storage-client','upload','--file', file_name, '--object-id', 'fbd35588-5bf8-560c-873a-0410f49e5748', '--md5', '039d75310a642d7ee2d2d8dfd7107c31'])
 
 except Exception, e:
     with open('jt.log', 'w') as f: f.write(str(e))
 
-    raise Exception  #for testing
+    #raise Exception  #for testing
     sys.exit(1)  # task failed
 
 # try:
