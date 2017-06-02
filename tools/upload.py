4a33cf957708ef98e632b07d2cd79c2bc407cd44
Chen2x <chenchen199727@gmail.com>
2017-06-02 11:30:25 -0400

    new version

--- tools/upload.py
+++ tools/upload.py
@@ -30,7 +30,7 @@ project_code = task_dict.get('input').get('project_code')
 task_start = int(time.time())
 
 try:
-    print subprocess.check_output(['icgc-storage-client','upload','--file', file_name, '--object-id', object_id, '--md5', file_md5sum, '--force'])
+    print subprocess.check_output(['icgc-storage-client','upload','--file', file_, '--object-id', object_id, '--md5', file_md5sum, '--force'])
     #auto icgc-storage-client upload --file test --object-id fbd35588-5bf8-560c-873a-0410f49e5748 --md5 d8e8fca2dc0f896fd7cb4cb0031ba249 --force
 except Exception, e:
     with open('jt.log', 'w') as f: f.write(str(e))
