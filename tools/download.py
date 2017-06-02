1fd6eec0f68bf2b613163c062e8dd401a937c433
Chen2x <chenchen199727@gmail.com>
2017-06-02 16:44:03 -0400

    xml

--- tools/download.py
+++ tools/download.py
@@ -7,7 +7,6 @@ import subprocess
 from utils import get_task_dict, save_output_json
 
 task_dict = get_task_dict(sys.argv[1])
-#task_dict = get_task_dict("""{"input": {"project_code": "","collab_file_id": "EGAF00000588185","file_name": "c9dc3c3fed697c072a3b00c9fe2c4490.CPCG_0047_Ly_R_PE_294_WG_111216_h801_0065_AD0813ACXX_6_NoIndex_R2.fastq.gz","file_md5sum": "c9dc3c3fed697c072a3b00c9fe2c4490","object_id": "7e2d1747-cda9-5843-8103-d6192cd55af4"}}""")
 cwd = os.getcwd()
 
 """
@@ -25,9 +24,8 @@ cwd = os.getcwd()
 """
 
 file_name = task_dict.get('input').get('file_name')
-file_md5sum = task_dict.get('input').get('file_md5sum')
 object_id = task_dict.get('input').get('object_id')
-project_code = task_dict.get('input').get('project_code')
+file_md5sum = task_dict.get('input').get('xml_file_md5sum')
 
 
 task_start = int(time.time())
@@ -37,18 +35,7 @@ try:
 
 except Exception, e:
     with open('jt.log', 'w') as f: f.write(str(e))
-
-    #raise Exception  #for testing
     sys.exit(1)  # task failed
-
-# try:
-#     r = subprocess.check_output(['curl','https://raw.githubusercontent.com/jt-hub/ega-collab-transfer-tools/master/download_ega_file.py','|','python','-','-p',project_code,'-f', ega_file_id+".aes", '-o', file_name])
-# except Exception, e:
-#     print e
-#     sys.exit(1)  # task failed
-
-
-# complete the task
 
 task_stop = int(time.time())
 
