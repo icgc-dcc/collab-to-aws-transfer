209ff8d895ca42f5aa95878a3df2f1b556c90c28
Chen2x <chenchen199727@gmail.com>
2017-06-07 14:40:42 -0400

    added delete v0.1.40

---
+++ tools/delete.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import time
+from utils import get_md5, get_task_dict, save_output_json
+
+task_dict = get_task_dict(sys.argv[1])
+
+"""
+    input:
+      file:
+        type: string
+"""
+file_ = task_dict.get('input').get('file')
+
+task_start = int(time.time())
+
+try:
+	os.remove(filename)
+except Exception, e:
+    with open('jt.log', 'w') as f: f.write(str(e))
+
+    sys.exit(1)  # task failed
+
+# try:
+#     r = subprocess.check_output(['curl','https://raw.githubusercontent.com/jt-hub/ega-collab-transfer-tools/master/download_ega_file.py','|','python','-','-p',project_code,'-f', ega_file_id+".aes", '-o', file_name])
+# except Exception, e:
+#     print e
+#     sys.exit(1)  # task failed
+
+
+# complete the task
+
+task_stop = int(time.time())
--- tools/upload.py
+++ tools/upload.py
@@ -4,11 +4,9 @@ import os
 import sys
 import time
 import subprocess
-from utils import get_md5
-from utils import get_task_dict, save_output_json
+from utils import get_md5, get_task_dict, save_output_json
 
 task_dict = get_task_dict(sys.argv[1])
-#task_dict = get_task_dict("""{"input": {"file": "/home/ubuntu/collab-to-aws-transfer-jt/tools/c9dc3c3fed697c072a3b00c9fe2c4490.CPCG_0047_Ly_R_PE_294_WG_111216_h801_0065_AD0813ACXX_6_NoIndex_R2.fastq.gz","collab_file_id": "EGAF00000588185","file_name": "c9dc3c3fed697c072a3b00c9fe2c4490.CPCG_0047_Ly_R_PE_294_WG_111216_h801_0065_AD0813ACXX_6_NoIndex_R2.fastq.gz","file_md5sum": "c9dc3c3fed697c072a3b00c9fe2c4490","object_id": "7e2d1747-cda9-5843-8103-d6192cd55af4"}}""")
 cwd = os.getcwd()
 
 """
@@ -30,7 +28,6 @@ file_ = task_dict.get('input').get('file')
 file_name = task_dict.get('input').get('file_name')
 file_md5sum = task_dict.get('input').get('file_md5sum')
 object_id = task_dict.get('input').get('object_id')
-#project_code = task_dict.get('input').get('project_code')
 
 if file_md5sum is None:
     file_md5sum = str(get_md5(file_))
@@ -39,11 +36,9 @@ task_start = int(time.time())
 
 try:
     print subprocess.check_output(['icgc-storage-client','upload','--file', file_, '--object-id', object_id, '--md5', file_md5sum, '--force'])
-    #icgc-storage-client upload --file test --object-id fbd35588-5bf8-560c-873a-0410f49e5748 --md5 d8e8fca2dc0f896fd7cb4cb0031ba249 --force
 except Exception, e:
     with open('jt.log', 'w') as f: f.write(str(e))
 
-    raise Exception  #for testing
     sys.exit(1)  # task failed
 
 # try:
@@ -59,6 +54,7 @@ task_stop = int(time.time())
 
 
 output_json = {
+    'file': file_
     'file_md5sum': file_md5sum,
     'runtime': {
         'task_start': task_start,
